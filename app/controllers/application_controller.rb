3db45c8f7e51792c616e0f502386fa8290278204
xyy601chenyan <xyy545688506@gmail.com>
2017-07-07 18:01:38 +0800

    fix sign in error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,12 @@ class ApplicationController < ActionController::Base
     added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
     added_params= [:name, :email, :password, :password_confirmation]
 
+
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_params
 
+
+  
  end
 
 end
